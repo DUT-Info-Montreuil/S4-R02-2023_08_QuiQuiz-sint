6cab101f50361365132e883a3577e183bf7895b1
TsukuyomiKun3 <onur.genc03@gmail.com>
2023-03-30 16:45:17 +0200

    Création des directory et ajout de la dépendance à Joueur_sme

---
+++ src/fr/iut/montreuil/S4_R02_2023_08/quiquiz_sint/test.java
@@ -0,0 +1,7 @@
+package fr.iut.montreuil.S4_R02_2023_08.quiquiz_sint;
+
+import fr.iut.montreuil.S4_RO2_2023_08.joueur_sme.entities.dto.JoueurDTO;
+
+public class test {
+    private JoueurDTO j;
+}
